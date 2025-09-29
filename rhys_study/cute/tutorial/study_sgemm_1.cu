df557bc023434468c7db4962059170a28a10556e
qinzhiqi <qinzhiqi@dg.com>
2025-09-29 23:58:35 +0800

    save

--- rhys_study/cute/tutorial/study_sgemm_1.cu
+++ rhys_study/cute/tutorial/study_sgemm_1.cu
@@ -1,15 +1,18 @@
 #include <cute/tensor.hpp>
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
+#include "cutlass/util/print_error.hpp"
+#include "cutlass/util/GPU_Clock.hpp"
 #include "cutlass/util/helper_cuda.hpp"
 
 template <class ProblemShape, class CtaTiler, class TA, class AStride, class ASmemLayout, class AThreadLayout,
           class TB, class BStride, class BSmemLayout, class BThreadLayout, class TC, class CStride, class CSmemLayout, class CThreadLayout>
 __global__ static __launch_bounds__(decltype(size(CThreadLayout{}))::value) void gemm_kernel(
-    ProblemShape shape_MNK, CtaTiler cta_tiler, TA const *A, AStride dA, ASmemLayout sA, AThreadLayout tA,
+    ProblemShape shape_MNK, CtaTiler cta_tiler, TA const *A, AStride dA, ASmemLayout sA_layout, AThreadLayout tA,
     TB const *B, BStride dB, BSmemLayout sB_layout, BThreadLayout tB,
     TC *C, CStride dC, CSmemLayout sC, CThreadLayout tC)
 {
+  using namespace cute;
   // check
   CUTE_STATIC_ASSERT_V(rank(shape_MNK) == Int<3>{});
   CUTE_STATIC_ASSERT_V(rank(cta_tiler) == Int<3>{});
@@ -46,6 +49,59 @@ __global__ static __launch_bounds__(decltype(size(CThreadLayout{}))::value) void
   CUTE_STATIC_ASSERT_V(congruent(select<0, 1>(shape_MNK), dC));       // dC strides for shape MN
 
   // make tensor
+  Tensor mA = make_tensor(make_gmem_ptr(A), select<0, 2>(shape_MNK), dA);
+  Tensor mB = make_tensor(make_gmem_ptr(B), select<1, 2>(shape_MNK), dB);
+  Tensor mC = make_tensor(make_gmem_ptr(C), select<0, 1>(shape_MNK), dC);
+
+  // get cta block
+
+  auto cta_coord = make_coord(blockIdx.x, blockIdx.y, _);
+  Tensor gA = local_tile(mA, cta_tiler, cta_coord, Step<_1, X, _1>{}); // (BLK_M, BLK_K, k)
+  Tensor gB = local_tile(mB, cta_tiler, cta_coord, Step<X, _1, _1>{}); // (BLK_N, BLK_K, k)
+  Tensor gC = local_tile(mC, cta_tiler, cta_coord, Step<_1, _1, X>{}); // (BLK_M, BLK_N)
+
+  // create smem
+  __shared__ TA smemA[cosize_v<ASmemLayout>];
+  __shared__ TB smemB[cosize_v<BSmemLayout>];
+  Tensor sA = make_tensor(make_smem_ptr(smemA), sA_layout); // (BLK_M, BLK_K)
+  Tensor sB = make_tensor(make_smem_ptr(smemB), sB_layout); // (BLK_N, BLK_K)
+
+  // how to move gA to sA?
+  // a thread copy a tile from gA to sA
+  Tensor tAgA = local_partition(gA, tA, threadIdx.x); // (THR_M, THR_K, k)
+  Tensor tAsA = local_partition(sA, tA, threadIdx.x); // (THR_M, THR_K)
+  // how to move gB to sB?
+  Tensor tBgB = local_partition(gB, tB, threadIdx.x); // (THR_N, THR_K, k)
+  Tensor tBsB = local_partition(sB, tB, threadIdx.x); // (THR_N, THR_K)
+
+  // how to do multiply and accumulate?
+  // Partition sA (BLK_M, BLK_K) by the rows of tC
+  Tensor tCsA = local_partition(sA, tC, threadIdx.x, Step<_1, X>{}); // (THR_M,BLK_K)
+  // Partition sB (BLK_N, BLK_K) by the cols of tC
+  Tensor tCsB = local_partition(sB, tC, threadIdx.x, Step<X, _1>{}); // (THR_N,BLK_K)
+  // Partition gC (M,N) by the tile of tC
+  Tensor tCgC = local_partition(gC, tC, threadIdx.x, Step<_1, _1>{}); // (THR_M,THR_N)
+
+  // Allocate the accumulators -- same shape/layout as the partitioned data
+  Tensor tCrC = make_tensor_like(tCgC); // (THR_M,THR_N)
+
+  // Clear the accumulators
+  clear(tCrC);
+
+  auto K_TILE_MAX = size<2>(tAgA);
+  for (int k_tile = 0; k_tile < K_TILE_MAX; ++k_tile)
+  {
+    copy(tAgA(_, _, k_tile), tAsA);
+    copy(tBgB(_, _, k_tile), tBsB);
+
+    cp_async_fence();   // Label the end of (potential) cp.async instructions
+    cp_async_wait<0>(); // Sync on all (potential) cp.async instructions
+    __syncthreads();    // Wait for all threads to write to smem
+
+    gemm(tCsA, tCsB, tCrC); // (THR_M,THR_N) += (THR_M,BLK_K) * (THR_N,BLK_K)
+    __syncthreads();        // Wait for all threads to read from smem
+  }
+  copy(tCrC, tCgC);
 }
 
 template <class TA, class TB, class TC>
