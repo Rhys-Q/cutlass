064985b855c30930cb922fed0bbc302419f9301c
qinzhiqi <qinzhiqi@dg.com>
2025-10-11 08:30:58 +0800

    save

--- rhys_study/cute/tutorial/layout.cu
+++ rhys_study/cute/tutorial/layout.cu
@@ -1,3 +1,4 @@
+#include "cute/int_tuple.hpp"
 #include "cute/util/type_traits.hpp"
 #include "cutlass/util/GPU_Clock.hpp"
 #include "cutlass/util/helper_cuda.hpp"
@@ -30,8 +31,72 @@ void test_integer() {
   std::cout << is_std_integral<Int<1>>() << std::endl; // false
   std::cout << is_std_integral<int>() << std::endl;    // true
 }
+void test_tuple() {
+  tuple<int, std::string> t1(1, "2sadsad");
+  std::cout << t1 << std::endl;
 
+  // get the first element
+  std::cout << get<0>(t1) << std::endl;
+  // get the second element
+  std::cout << get<1>(t1) << std::endl;
+
+  // make a tuple
+  auto t2 = make_tuple(1, 2.0, 21);
+  std::cout << t2 << std::endl;
+
+  // malloc a tuple
+  auto t3 = new tuple<int, float>(1, 2.0);
+  std::cout << *t3 << std::endl;
+
+  // free the tuple
+  delete t3;
+}
+
+void test_int_tuple() {
+  std::vector<int> a = {6, 3, 4};
+  auto tup = make_int_tuple<5>(a, a.size(), 0); // (6,3,4,0,0)
+  std::cout << tup << std::endl;
+
+  // get the first element
+  std::cout << get<0>(tup) << std::endl;
+  // get the second element
+  std::cout << get<1>(tup) << std::endl;
+
+  // make a nested tuple
+  auto tup2 = make_tuple(make_tuple(1, 2.5), make_tuple(3, 4.0), 5);
+
+  std::cout << get<0, 0>(tup2) << std::endl;
+  std::cout << get<0, 1>(tup2) << std::endl;
+  std::cout << get<1, 0>(tup2) << std::endl;
+  std::cout << get<1, 1>(tup2) << std::endl;
+  std::cout << get<2>(tup2) << std::endl;
+
+  auto tup3 =
+      make_tuple(uint16_t{42}, make_tuple(Int<1>{}, int32_t{3}), Int<17>{});
+  std::cout << get<0>(tup3) << std::endl;
+  std::cout << get<1, 0>(tup3) << std::endl;
+  std::cout << get<1, 1>(tup3) << std::endl;
+  std::cout << get<2>(tup3) << std::endl;
+
+  // rank
+  std::cout << rank(tup3) << std::endl;
+  std::cout << rank<0>(tup3) << std::endl;
+  std::cout << rank<1>(tup3) << std::endl;
+  std::cout << rank<2>(tup3) << std::endl;
+
+  // depth
+  std::cout << depth(tup3) << std::endl;
+  std::cout << depth<0>(tup3) << std::endl;
+  std::cout << depth<1>(tup3) << std::endl;
+  std::cout << depth<2>(tup3) << std::endl;
+
+  // size
+  std::cout << size(tup3) << std::endl;
+  std::cout << size<0>(tup3) << std::endl;
+  std::cout << size<1>(tup3) << std::endl;
+  std::cout << size<2>(tup3) << std::endl;
+}
 int main() {
-  test_dynamic_integer();
+  test_int_tuple();
   return 0;
 }
